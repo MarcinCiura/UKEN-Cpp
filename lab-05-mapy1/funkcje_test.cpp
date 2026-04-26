0e5568c0d41fb1e923fc86562126aa5eca82a91b
Marcin Ciura <mciura@gmail.com>
2026-04-26 13:15:36 +0200

    Zmiany

--- lab-05-mapy1/funkcje_test.cpp
+++ lab-05-mapy1/funkcje_test.cpp
@@ -13,108 +13,109 @@ using namespace std;
 namespace {
 
 TEST(WczytajStudentowTest, WczytujePoprawnie) {
-    stringstream wejscie{
-        R"(Eryk    Klimek  903072088
+  stringstream wejscie{
+      R"(Eryk    Klimek  903072088
 Henryk  Wozniak 682759817
 )"};
-    map<int, Student> oczekiwane = {
-        {903072088, {.imie = "Eryk", .nazwisko = "Klimek"}},
-        {682759817, {.imie = "Henryk", .nazwisko = "Wozniak"}},
-    };
-    map<int, Student> wynik = wczytaj_studentow(wejscie);
-    ASSERT_EQ(wynik.size(), oczekiwane.size());
-    for (const auto& [klucz, student] : wynik) {
-        ASSERT_EQ(student.imie, oczekiwane[klucz].imie);
-        ASSERT_EQ(student.nazwisko, oczekiwane[klucz].nazwisko);
-        ASSERT_EQ(student.oceny_z_przedmiotow, oczekiwane[klucz].oceny_z_przedmiotow);
-    }
+  map<int, Student> oczekiwane = {
+      {903072088, {.imie = "Eryk", .nazwisko = "Klimek"}},
+      {682759817, {.imie = "Henryk", .nazwisko = "Wozniak"}},
+  };
+  map<int, Student> wynik = wczytaj_studentow(wejscie);
+  ASSERT_EQ(wynik.size(), oczekiwane.size());
+  for (const auto [klucz, student] : wynik) {
+    ASSERT_EQ(student.imie, oczekiwane[klucz].imie);
+    ASSERT_EQ(student.nazwisko, oczekiwane[klucz].nazwisko);
+    ASSERT_EQ(student.oceny_z_przedmiotow,
+              oczekiwane[klucz].oceny_z_przedmiotow);
+  }
 }
 
 TEST(WczytajOcenyTest, WczytujePoprawnie) {
-    map<int, Student> studenci = {
-        {903, {.imie = "Eryk", .nazwisko = "Klimek"}},
-        {682, {.imie = "Henryk", .nazwisko = "Wozniak"}},
-    };
-    stringstream oceny{
-        R"(682       sieci_komputerowe       4.5
+  map<int, Student> studenci = {
+      {903, {.imie = "Eryk", .nazwisko = "Klimek"}},
+      {682, {.imie = "Henryk", .nazwisko = "Wozniak"}},
+  };
+  stringstream oceny{
+      R"(682       sieci_komputerowe       4.5
         682       programowanie   2.5     3.5
 )"};
-    map<int, Student> oczekiwane = {
-        {903, {"Eryk", "Klimek", {}}},
-        {682,
-         {"Henryk",
-          "Wozniak",
-          {
-              {"sieci_komputerowe", {4.5}},
-              {"programowanie", {2.5, 3.5}},
-          }}},
-    };
-    wczytaj_oceny(oceny, &studenci);
-    ASSERT_EQ(studenci.size(), oczekiwane.size());
-    for (const auto& [klucz, student] : studenci) {
-        ASSERT_EQ(student.imie, oczekiwane[klucz].imie);
-        ASSERT_EQ(student.nazwisko, oczekiwane[klucz].nazwisko);
-        ASSERT_EQ(student.oceny_z_przedmiotow, oczekiwane[klucz].oceny_z_przedmiotow);
-    }
+  map<int, Student> oczekiwane = {
+      {903, {"Eryk", "Klimek", {}}},
+      {682,
+       {"Henryk",
+        "Wozniak",
+        {
+            {"sieci_komputerowe", {4.5}},
+            {"programowanie", {2.5, 3.5}},
+        }}},
+  };
+  wczytaj_oceny(oceny, &studenci);
+  ASSERT_EQ(studenci.size(), oczekiwane.size());
+  for (const auto [klucz, student] : studenci) {
+    ASSERT_EQ(student.imie, oczekiwane[klucz].imie);
+    ASSERT_EQ(student.nazwisko, oczekiwane[klucz].nazwisko);
+    ASSERT_EQ(student.oceny_z_przedmiotow,
+              oczekiwane[klucz].oceny_z_przedmiotow);
+  }
 }
 
 TEST(WyznaczNajpopularniejszeImieTest, DzialaPoprawnie) {
-    map<int, Student> studenci = {
-        {903, {.imie = "Eryk", .nazwisko = "Klimek"}},
-        {682, {.imie = "Henryk", .nazwisko = "Wozniak"}},
-        {783, {.imie = "Eryk", .nazwisko = "Nowak"}},
-    };
-    ASSERT_EQ(wyznacz_najpopularniejsze_imie(studenci), "Eryk");
+  map<int, Student> studenci = {
+      {903, {.imie = "Eryk", .nazwisko = "Klimek"}},
+      {682, {.imie = "Henryk", .nazwisko = "Wozniak"}},
+      {783, {.imie = "Eryk", .nazwisko = "Nowak"}},
+  };
+  ASSERT_EQ(wyznacz_najpopularniejsze_imie(studenci), "Eryk");
 }
 
 TEST(WyznaczNajpopularniejszeNazwiskoTest, DzialaPoprawnie) {
-    map<int, Student> studenci = {
-        {903, {.imie = "Eryk", .nazwisko = "Klimek"}},
-        {682, {.imie = "Henryk", .nazwisko = "Wozniak"}},
-        {384, {.imie = "Jan", .nazwisko = "Wozniak"}},
-    };
-    pair<string, int> oczekiwane{"Wozniak", 2};
-    ASSERT_EQ(wyznacz_najpopularniejsze_nazwisko(studenci), oczekiwane);
+  map<int, Student> studenci = {
+      {903, {.imie = "Eryk", .nazwisko = "Klimek"}},
+      {682, {.imie = "Henryk", .nazwisko = "Wozniak"}},
+      {384, {.imie = "Jan", .nazwisko = "Wozniak"}},
+  };
+  pair<string, int> oczekiwane{"Wozniak", 2};
+  ASSERT_EQ(wyznacz_najpopularniejsze_nazwisko(studenci), oczekiwane);
 }
 
 TEST(WypiszStudentowIIchOcenyTest, WypisujePoprawnie) {
-    map<int, Student> studenci = {
-        {903, {"Eryk", "Klimek", {}}},
-        {682,
-         {"Henryk",
-          "Wozniak",
-          {
-              {"sieci_komputerowe", {4.5}},
-              {"programowanie", {2.5, 3.5}},
-          }}},
-    };
-    stringstream wynik;
-    wypisz_studentow_i_ich_oceny(studenci, wynik);
-    string oczekiwane =
-        "Henryk Wozniak (682)\n"
-        "\tprogramowanie\t2.5 3.5 \n"
-        "\tsieci_komputerowe\t4.5 \n"
-        "Eryk Klimek (903)\n";
-    ASSERT_EQ(wynik.str(), oczekiwane);
+  map<int, Student> studenci = {
+      {903, {"Eryk", "Klimek", {}}},
+      {682,
+       {"Henryk",
+        "Wozniak",
+        {
+            {"sieci_komputerowe", {4.5}},
+            {"programowanie", {2.5, 3.5}},
+        }}},
+  };
+  stringstream wynik;
+  wypisz_studentow_i_ich_oceny(studenci, wynik);
+  string oczekiwane = "Henryk Wozniak (682)\n"
+                      "\tprogramowanie\t2.5 3.5 \n"
+                      "\tsieci_komputerowe\t4.5 \n"
+                      "Eryk Klimek (903)\n";
+  ASSERT_EQ(wynik.str(), oczekiwane);
 }
 
 TEST(PoliczStudentowBezOcenTest, DzialaPoprawnie) {
   map<int, Student> studenci = {
-    {903, {"Eryk", "Klimek", {}}},
-    {682,
-     {"Henryk",
-      "Wozniak",
-      {
-        {"sieci_komputerowe", {4.5}},
-        {"programowanie", {2.5, 3.5}},
-      }}},
+      {903, {"Eryk", "Klimek", {}}},
+      {682,
+       {"Henryk",
+        "Wozniak",
+        {
+            {"sieci_komputerowe", {4.5}},
+            {"programowanie", {2.5, 3.5}},
+        }}},
   };
   ASSERT_EQ(policz_studentow_bez_ocen(studenci), 1);
 }
 
-}  // namespace
+} // namespace
 
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- lab-05-mapy1/main.cpp
+++ lab-05-mapy1/main.cpp
@@ -10,60 +10,62 @@
 
 using namespace std;
 
-static void zglos_blad(const string& nazwa_pliku) {
-    cerr << "Nie udało się otworzyć pliku " << nazwa_pliku << endl;
-    cerr << "Przyczyna: " << strerror(errno) << endl;
-    exit(1);
+static void zglos_blad(const string &nazwa_pliku) {
+  cerr << "Nie udało się otworzyć pliku " << nazwa_pliku << endl;
+  cerr << "Przyczyna: " << strerror(errno) << endl;
+  exit(1);
 }
 
-static ifstream otworz_plik_do_odczytu(const string& nazwa_pliku) {
-    auto strumien = ifstream(nazwa_pliku);
-    if (!strumien) {
-        zglos_blad(nazwa_pliku);
-    }
-    return strumien;
+static ifstream otworz_plik_do_odczytu(const string &nazwa_pliku) {
+  auto strumien = ifstream(nazwa_pliku);
+  if (!strumien) {
+    zglos_blad(nazwa_pliku);
+  }
+  return strumien;
 }
 
-static ofstream otworz_plik_do_zapisu(const string& nazwa_pliku) {
-    auto strumien = ofstream(nazwa_pliku);
-    if (!strumien) {
-      zglos_blad(nazwa_pliku);
-    }
-    return strumien;
+static ofstream otworz_plik_do_zapisu(const string &nazwa_pliku) {
+  auto strumien = ofstream(nazwa_pliku);
+  if (!strumien) {
+    zglos_blad(nazwa_pliku);
+  }
+  return strumien;
 }
 
 int main() {
-    const string NAZWA_PLIKU_STUDENCI{"studenci.txt"};
-    const string NAZWA_PLIKU_OCENY{"oceny.txt"};
-    const int LICZBA_STUDENTOW{1000};
-    vector<int> pesele;
-    map<int, Student> studenci;
-    {
-        ofstream strumien = otworz_plik_do_zapisu(NAZWA_PLIKU_STUDENCI);
-        pesele = tworz_studentow(strumien, LICZBA_STUDENTOW);
-        strumien.close();
-    }
-    {
-        ofstream strumien = otworz_plik_do_zapisu(NAZWA_PLIKU_OCENY);
-        tworz_oceny(pesele, strumien);
-        strumien.close();
-    }
-    {
-        ifstream strumien = otworz_plik_do_odczytu(NAZWA_PLIKU_STUDENCI);
-        studenci = wczytaj_studentow(strumien);
-        strumien.close();
-    }
-    string najpopularniejsze_imie = wyznacz_najpopularniejsze_imie(studenci);
-    cout << "najpopularniejsze imie: " << najpopularniejsze_imie << endl;
+  const string NAZWA_PLIKU_STUDENCI{"studenci.txt"};
+  const string NAZWA_PLIKU_OCENY{"oceny.txt"};
+  const int LICZBA_STUDENTOW{1000};
+  vector<int> pesele;
+  map<int, Student> studenci;
+  {
+    ofstream strumien = otworz_plik_do_zapisu(NAZWA_PLIKU_STUDENCI);
+    pesele = tworz_studentow(strumien, LICZBA_STUDENTOW);
+    strumien.close();
+  }
+  {
+    ofstream strumien = otworz_plik_do_zapisu(NAZWA_PLIKU_OCENY);
+    tworz_oceny(pesele, strumien);
+    strumien.close();
+  }
+  {
+    ifstream strumien = otworz_plik_do_odczytu(NAZWA_PLIKU_STUDENCI);
+    studenci = wczytaj_studentow(strumien);
+    strumien.close();
+  }
+  string najpopularniejsze_imie = wyznacz_najpopularniejsze_imie(studenci);
+  cout << "najpopularniejsze imie: " << najpopularniejsze_imie << endl;
 
-    auto [najpopularniejsze_nazwisko, liczba_osob] = wyznacz_najpopularniejsze_nazwisko(studenci);
-    cout << "najpopularniejsze nazwisko: " << najpopularniejsze_nazwisko << '\t' << liczba_osob
-         << endl;
-    {
-        ifstream strumien = otworz_plik_do_odczytu(NAZWA_PLIKU_OCENY);
-        wczytaj_oceny(strumien, &studenci);
-        strumien.close();
-    }
-    wypisz_studentow_i_ich_oceny(studenci, cout);
-    cout << "liczba studentow bez ocen: " << policz_studentow_bez_ocen(studenci) << endl;
+  const auto [najpopularniejsze_nazwisko, liczba_osob] =
+      wyznacz_najpopularniejsze_nazwisko(studenci);
+  cout << "najpopularniejsze nazwisko: " << najpopularniejsze_nazwisko << '\t'
+       << liczba_osob << endl;
+  {
+    ifstream strumien = otworz_plik_do_odczytu(NAZWA_PLIKU_OCENY);
+    wczytaj_oceny(strumien, &studenci);
+    strumien.close();
+  }
+  wypisz_studentow_i_ich_oceny(studenci, cout);
+  cout << "liczba studentow bez ocen: " << policz_studentow_bez_ocen(studenci)
+       << endl;
 }
