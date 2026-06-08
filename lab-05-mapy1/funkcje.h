328f1a219e848046c24c130b8c99ab5145f8c61f
Marcin Ciura <mciura@gmail.com>
2026-06-08 16:52:12 +0200

    Dodano komentarz

--- lab-05-mapy1/funkcje.h
+++ lab-05-mapy1/funkcje.h
@@ -8,30 +8,33 @@
 
 // Struktura reprezentuje studenta i jego/jej oceny.
 struct Student {
-    std::string imie, nazwisko;
-    std::map<std::string, std::vector<double>> oceny_z_przedmiotow;
+  std::string imie, nazwisko;
+  // Nazwa przedmiotu -> wektor ocen z tego przedmiotu
+  std::map<std::string, std::vector<double>> oceny_z_przedmiotow;
 };
 
 // Funkcja generuje dane studentow i zapisuje je do strumienia.
 // @param strumien strumień, do ktorego funkcja zapisuje dane studentów
 // @param N ile danych zapisać?
 // @return pesele z wygenerowanych danych
-std::vector<int> tworz_studentow(std::ostream& strumien, const int N);
+std::vector<int> tworz_studentow(std::ostream &strumien, const int N);
 
 // Funkcja generuje oceny studentow i zapisuje je do strumienia.
 // @param strumien strumień, do ktorego funkcja zapisuje oceny studentów
 // @param pesele pesele studentów
-void tworz_oceny(const std::vector<int>& pesele, std::ostream& strumien);
+void tworz_oceny(const std::vector<int> &pesele, std::ostream &strumien);
 
-std::map<int, Student> wczytaj_studentow(std::istream& strumien);
+std::map<int, Student> wczytaj_studentow(std::istream &strumien);
 
-void wczytaj_oceny(std::istream& strumien, std::map<int, Student>* studenci);
+void wczytaj_oceny(std::istream &strumien, std::map<int, Student> *studenci);
 
-std::string wyznacz_najpopularniejsze_imie(const std::map<int, Student>& studenci);
+std::string
+wyznacz_najpopularniejsze_imie(const std::map<int, Student> &studenci);
 
-std::pair<std::string, int> wyznacz_najpopularniejsze_nazwisko(
-    const std::map<int, Student>& studenci);
+std::pair<std::string, int>
+wyznacz_najpopularniejsze_nazwisko(const std::map<int, Student> &studenci);
 
-void wypisz_studentow_i_ich_oceny(const std::map<int, Student>& studenci, std::ostream& strumien);
+void wypisz_studentow_i_ich_oceny(const std::map<int, Student> &studenci,
+                                  std::ostream &strumien);
 
-int policz_studentow_bez_ocen(const std::map<int, Student>& studenci);
+int policz_studentow_bez_ocen(const std::map<int, Student> &studenci);
